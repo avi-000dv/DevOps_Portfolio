80b86b977447320856ce5237936772d59b8bde3a
Abegail Padit <syntaxletmein@gmail.com>
2025-05-28 11:28:20 +0800

    up
    titeng mataba

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "John Layda's Portfolio",
+  title: "TITENG MATABA",
   description: "DevOps Engineer & Entrepreneur",
 }
 
